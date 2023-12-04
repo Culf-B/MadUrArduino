3acaf41b2762b0cfec4546e2318465bc9b19e20c
Culf-B <49363745+Culf-B@users.noreply.github.com>
2023-12-04 11:49:24 +0100

    Created clock function

--- MadUrArduino.ino
+++ MadUrArduino.ino
@@ -1,5 +1,7 @@
 #include <Encoder.h>
 #include <Wire.h>
+#include "DS1307.h"
+#include "rgb_lcd.h"
 
 // Global vars for ButtonEvent
 const int buttonPin = 6;
@@ -14,11 +16,28 @@ int encoderValue = 0;
 int prevEncoderValueCount = 0;
 int encoderValueCount = 0;
 
+// LCD definition and variables
+rgb_lcd lcd;
+const int colorR = 255;
+const int colorG = 255;
+const int colorB = 255;
+
+// Clock definition
+DS1307 clock; //define a object of DS1307 class
+
 void setup()
 {
 	Serial.begin(9600);
 
+	// Set button pin
 	pinMode(buttonPin, INPUT);
+
+	// Clock setup
+	clock.begin();
+
+	// LCD setup
+    lcd.begin(16, 2);
+    lcd.setRGB(colorR, colorG, colorB);
 }
 
 void loop()
@@ -29,6 +48,41 @@ void loop()
 
 	// Update buttonClicked
 	ButtonEvent();
+}
+
+void Clock()
+{
+	// Get current time from RTC
+	clock.getTime();
+	// Print time on lcd
+	lcd.setCursor(0, 0);
+	lcd.print(SetZero(clock.hour));
+	lcd.print(clock.hour);
+	lcd.print(":");
+	lcd.print(SetZero(clock.minute));
+	lcd.print(clock.minute);
+	lcd.print(":");
+	lcd.print(SetZero(clock.second));
+	lcd.print(clock.second);
+	// Print date on lcd
+	lcd.setCursor(0, 1);
+	lcd.print(SetZero(clock.dayOfMonth));
+	lcd.print(clock.dayOfMonth);
+	lcd.print("/");
+	lcd.print(SetZero(clock.month));
+	lcd.print(clock.month);
+	lcd.print("/");
+	lcd.print(clock.year+2000);
+}
+
+String SetZero(uint8_t number)
+{
+	if (number > 9)
+	{
+		return "";
+	} else {
+		return "0";
+	}
 }
 
 void ButtonEvent() 
